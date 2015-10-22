7bfd3ba0b09e9757e56b67e766a3652e82569f12
Ganesh Narayanan <ganara@microsoft.com>
2015-10-21 20:22:16 -0700

    Added Stylecop settings

---
+++ Entrevista/Basic/Sorting/AbstractSort.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace GaneshNarayanan.Entrevista.Basic.Sorting
+{
+    public abstract class AbstractSort<T>
+    {
+        public abstract void SelectionSort(T[] arrayToSort, Comparer<T> comparer = null);
+
+        /// <summary>The swap.</summary>
+        /// <param name="a">The a.</param>
+        /// <param name="b">The b.</param>
+        public static void Swap(ref T a, ref T b)
+        {
+            var t = b;
+            b = a;
+            a = t;
+        }
+    }
+}
